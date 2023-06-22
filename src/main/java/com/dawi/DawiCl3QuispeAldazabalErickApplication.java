28e8f0f1e4ed112695e5a41f7f9c6dae6fb421c4
ErickG <qaerickg@gmail.com>
2023-06-21 21:04:21 -0500

    1er commit

---
+++ src/main/java/com/dawi/DawiCl3QuispeAldazabalErickApplication.java
@@ -0,0 +1,13 @@
+package com.dawi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawiCl3QuispeAldazabalErickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawiCl3QuispeAldazabalErickApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dawi/DawiCl3QuispeAldazabalErickApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dawi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawiCl3QuispeAldazabalErickApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
